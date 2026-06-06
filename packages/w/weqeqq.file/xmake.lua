eac0d2ab1c1290c97a84cdfc228094c034f71332
weqeqq <ggmodwear@gmail.com>
2026-06-06 12:47:17 +0300

    feat: add weqeqq.file package definition

---
+++ packages/w/weqeqq.file/xmake.lua
@@ -0,0 +1,8 @@
+package("weqeqq.file")
+  add_urls("https://github.com/weqeqq/wqfile.git")
+  add_versions("c03e10a", "c03e10a7b4b4b71a49ffa30994738a78a6f4d3cb")
+
+  on_install(function (package)
+    import("package.tools.xmake").install(package)
+  end)
+package_end()
