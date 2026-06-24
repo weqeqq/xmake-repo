d03663729f9d10f713974af9efc43a63d6f36ab9
weqeqq <ggmodwear@gmail.com>
2026-06-25 01:44:35 +0300

    feat: add weqeqq.test package definition with core and main components

---
+++ packages/w/weqeqq.test/xmake.lua
@@ -0,0 +1,24 @@
+---@diagnostic disable
+-- stylua: ignore start
+
+package("weqeqq.test")
+  add_urls("https://github.com/weqeqq/wqtest.git")
+  add_versions("0.1.0", "3052482e29240a32ec6063a0958434ce7d439299")
+
+  add_components("core", { default = true })
+  add_components("main")
+
+  on_install(function(package)
+	  import("package.tools.xmake").install(package)
+  end)
+
+  on_component(function (package, component) 
+    local name = component:name() 
+    if name == "core" then 
+      component:add("links", "weqeqq.test")
+    elseif name == "main" then 
+      component:add("links", "weqeqq.test.main")
+      component:add("deps", "core")
+    end
+  end)
+package_end()
