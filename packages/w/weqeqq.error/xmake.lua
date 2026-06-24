e8df3fc1689c62c112cb30856fb6db92443f0156
weqeqq <ggmodwear@gmail.com>
2026-06-24 12:14:21 +0300

    feat: add version 0.1.1 and update versioning format for weqeqq.error

--- packages/w/weqeqq.error/xmake.lua
+++ packages/w/weqeqq.error/xmake.lua
@@ -1,8 +1,12 @@
+---@diagnostic disable
+-- stylua: ignore start
+
 package("weqeqq.error")
   add_urls("https://github.com/weqeqq/wqerror.git")
-  add_versions("cf7c58b", "e82887e70e2199e18570c2747b6ed6f95e74bb22")
+  add_versions("0.1.0", "e82887e70e2199e18570c2747b6ed6f95e74bb22")
+  add_versions("0.1.1", "227d2be5478417dbae721d99fefa7e9fdb66466a")
 
-  on_install(function (package)
-    import("package.tools.xmake").install(package)
+  on_install(function(package)
+	  import("package.tools.xmake").install(package)
   end)
 package_end()
