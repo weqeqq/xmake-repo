af9b515bf083a06c1c354afc1c31a7427de0c0d9
weqeqq <ggmodwear@gmail.com>
2026-06-25 07:26:39 +0300

    feat: add version 0.3.4 to weqeqq.test package definition

--- packages/w/weqeqq.test/xmake.lua
+++ packages/w/weqeqq.test/xmake.lua
@@ -9,6 +9,7 @@ package("weqeqq.test")
   add_versions("0.3.1", "v0.3.1")
   add_versions("0.3.2", "v0.3.2")
   add_versions("0.3.3", "v0.3.3")
+  add_versions("0.3.4", "v0.3.4")
 
   add_components("core", { default = true })
   add_components("main")
