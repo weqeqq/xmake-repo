78f9f066fc9a4b289e7daf605ccebb5ceb823a59
weqeqq <ggmodwear@gmail.com>
2026-06-25 05:17:55 +0300

    feat: add weqeqq.cli package definition to support wqcli installation

---
+++ packages/w/weqeqq.cli/xmake.lua
@@ -0,0 +1,11 @@
+---@diagnostic disable
+-- stylua: ignore start
+
+package("weqeqq.cli")
+  add_urls("https://github.com/weqeqq/wqcli.git")
+  add_versions("0.1.0", "v0.1.0")
+
+  on_install(function(package)
+	  import("package.tools.xmake").install(package)
+  end)
+package_end()
