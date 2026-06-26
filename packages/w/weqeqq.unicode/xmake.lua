b99e0d8e5b3006277bbee8481d59f241692cb144
weqeqq <ggmodwear@gmail.com>
2026-06-26 10:16:59 +0300

    feat: add version 0.1.1 and disable diagnostics for weqeqq.unicode package

--- packages/w/weqeqq.unicode/xmake.lua
+++ packages/w/weqeqq.unicode/xmake.lua
@@ -1,6 +1,11 @@
+
+---@diagnostic disable
+-- stylua: ignore start
+
 package("weqeqq.unicode")
   add_urls("https://github.com/weqeqq/wqunicode.git")
   add_versions("44eedb2", "44eedb2264555310642eb9fcc423cef6b4549b63")
+  add_versions("0.1.1", "v0.1.1")
 
   on_install(function (package)
     import("package.tools.xmake").install(package)
