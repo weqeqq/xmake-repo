138d809dbcaa755cc61ef86ff2ce7f15fe86f9d7
weqeqq <ggmodwear@gmail.com>
2026-06-26 09:32:06 +0300

    feat: add version 0.1.0 to weqeqq.file and update file formatting

--- packages/w/weqeqq.file/xmake.lua
+++ packages/w/weqeqq.file/xmake.lua
@@ -1,8 +1,13 @@
-package("weqeqq.file")
-  add_urls("https://github.com/weqeqq/wqfile.git")
-  add_versions("c03e10a", "c03e10a7b4b4b71a49ffa30994738a78a6f4d3cb")
 
-  on_install(function (package)
-    import("package.tools.xmake").install(package)
-  end)
+---@diagnostic disable
+-- stylua: ignore start
+
+package("weqeqq.file")
+    add_urls("https://github.com/weqeqq/wqfile.git")
+    add_versions("c03e10a", "c03e10a7b4b4b71a49ffa30994738a78a6f4d3cb")
+    add_versions("0.1.0", "v0.1.0")
+
+    on_install(function (package)
+        import("package.tools.xmake").install(package)
+    end)
 package_end()
